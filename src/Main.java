376e7e7ce8432795fbc44ebe197b1b11aae15681
SerMURZIK <118277030+SerMURZIK@users.noreply.github.com>
2023-05-03 22:31:44 +0400

    1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import ru.netology.finances.Finances;
 class Main {
 
     public static void main(String[] args) {
-        int payment = Finances.calcPayment(2600000, 200000, 2);
+        int payment = Finances.calcPayment(2_600_000, 200_000, 2);
         System.out.println("Ежемесячный платеж составит: " + payment);
-
     }
 }
--- src/ru/netology/finances/Finances.java
+++ src/ru/netology/finances/Finances.java
@@ -2,8 +2,7 @@ package ru.netology.finances; //Указали пакет
 
 public class Finances {
     public static int calcPayment(int amount, int start, int years) {
-
-        return (amount - start) / (years*12); //формула расчета
+        return (amount - start) / (years * 12); //формула расчета
     }
 }
 
